9a158f939bd07026f512ed4458ebf99e61bb9fb5
ichiragkumar <chiragroy3030@gmail.com>
2024-06-02 16:41:47 +0530

    what we do section is ready

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import mainHero from "../assets/hero.png"
 const MainPage = () => {
   return (
-    <div className='flex justify-between ml-[240px] mr-[30px] mt-[50px] w-[920px] '>
+    <div className='flex justify-between ml-[210px] mr-[30px] mt-[50px] max-w-[1140px]'>
             <div className='flex flex-col justify-center'>
-                <div className='text-[#FF3147] text-xl'>
-                    <h2>Award Wining </h2>
+                <div className='text-[#FF3147] text-xl font-medium' >
+                    <h2>AWARD WINNING </h2>
                 </div>
                 <div className='text-black text-4xl font-bold mt-10'>
                     <h2>DIGITAL MARKETING <br/> AGENCY</h2>
                 </div>
-                <div className='mt-7'>
+                <div className='mt-7 text-xl' >
                     <p>
                     Morbi sed lacus nec risus finibus feugiat et fermentum nibh. 
                     Pellentesque vitae ante at elit fringilla ac at purus, 
@@ -23,7 +23,7 @@ const MainPage = () => {
                 </div>
             </div>
             <div className='m-10'> 
-                <img src={mainHero} className='w-[587px]  h-[537px]' alt="Not Found" />
+                <img src={mainHero} className='w-[1187px]  h-[537px]' alt="Not Found" />
             </div>
     </div>
   )
--- src/components/WhatweDo.jsx
+++ src/components/WhatweDo.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
+import service1 from "../assets/service1.png"
+import service2 from "../assets/service2.png"
+import service3 from "../assets/service3.png"
 
 const WhatWeDoSection = () => {
   return (
-    <div className='max-w-[1920px] max-h-[1030px] bg-gray-100 ml-[240px] mr-[30px]' > 
-        <h2 className='text-red-700'>WHAT WE DO</h2>
-        <div className='flex justify-between mt-10'>
+    <div className='max-w-[1920px] max-h-[1030px]  ml-[210px] mr-[30px]' > 
+        <h2 className='text-red-700 font-medium text-xl'>WHAT WE DO</h2>
+        <div className='flex justify-between mt-10 gap-5'>
+
             <div className='text-black text-2xl font-bold '>
-                <h2>SERVICES  PROVIDE  <br/> FOR  YOU </h2>
-                </div>
-            <div>
+                <h2>SERVICES  PROVIDE <br/>
+                    FOR  YOU </h2>
+            </div>
+
+
+            <div className='mr-[200px] text-xl '>
                 <p>
-                    Morbi sed lacus nec risus finibus feugiat et fermentum nibh. 
-                    Pellentesque vitae ante at elit fringilla ac at purus, 
-                    Morbi sed lacus nec risus finibus feugiat et fermentum
+                    Morbi sed lacus nec risus finibus feugiat et fermentum nibh. Pellentesque <br/>
+                    vitae ante at elit fringilla ac at purus, 
+                    Morbi sed lacus nec risus finibus <br/>
+                    feugiat et fermentum
                 </p>
             </div>
+        </div>
+
+        <div className='flex justify-evenly mr-[250px] mt-10 gap-1'>
+            <div><img src={service1} alt="" /></div>
+            <div><img src={service2} alt="" /></div>
+            <div><img src={service3} alt="" /></div>
+            <div><img src={service2} alt="" /></div>
         </div>
     </div>
   )
